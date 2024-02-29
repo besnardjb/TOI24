deaa007c6b1f55b6c864da56769cae102e819f96
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2024-02-29 21:50:27 +0100

    Cours2

--- C1-Bases/TP/ex3/main.c
+++ C1-Bases/TP/ex3/main.c
@@ -9,11 +9,25 @@ int main(int argc, char ** argv)
 {
 	struct morpion m;
 
+	char buff[512];
+
+
 	morpion_init(&m);
 
-	morpion_set(&m, 2,1, 'X');
 
-	morpion_display(&m);
+
+	while(1)
+	{
+		fgets(buff, 512, stdin);
+
+		printf("%s", buff);
+
+
+		morpion_set(&m, 2,1, 'X');
+
+		morpion_display(&m);
+
+	}
 
 	return 0;
 }
